b986a2b1740dbd798a8d61eb45da6fb4173ed341
Sebastian Weigelt <weigelt@kit.edu>
2021-01-13 17:49:41 +0100

    add 'test'

---
+++ src/test/java/edu/kit/ipd/parse/ast_synth/patterns/ConditionFinderTest.java
@@ -0,0 +1,27 @@
+package edu.kit.ipd.parse.ast_synth.patterns;
+
+import edu.kit.ipd.parse.conditionDetection.CommandType;
+import edu.kit.ipd.parse.luna.graph.*;
+import org.junit.Test;
+
+public class ConditionFinderTest {
+
+	@Test
+	public void testPresence() {
+		IGraph pG = new ParseGraph();
+		INodeType nt = pG.createNodeType("token");
+		nt.addAttributeToType(CommandType.class.getName(), "commandType");
+		INode node = pG.createNode(nt);
+		node.setAttributeValue("commandType", CommandType.IF_STATEMENT);
+		switch ((String) node.getAttributeValue("commandType").toString()) {
+		case "IF":
+			System.out.println((String) node.getAttributeValue("commandType").toString());
+			break;
+		default:
+			System.out.println("fail");
+			break;
+		//		ConditionFinder cf = new ConditionFinder();
+		//		cf.extractPattern(pG);
+		}
+	}
+}
